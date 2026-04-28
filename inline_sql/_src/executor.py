c6d9275ec03f57e0c4a89053259dbe8c942e2705
shubhampawar-ethara <shubham.pawar@ethara.ai>
2026-04-28 14:52:27 +0530

    Commit 0

--- inline_sql/_src/executor.py
+++ inline_sql/_src/executor.py
@@ -24,23 +24,8 @@ class InlineSQL(Generic[T]):
     """
 
     def __init__(self, scalar: bool) -> None:
-        self.scalar = scalar
+        pass
 
     def __xor__(self, query: str) -> T:
         """Run an inline SQL query."""
-        frame = inspect.currentframe()
-        try:
-            f_locals, f_globals = frame.f_back.f_locals, frame.f_back.f_globals
-            context = {**f_globals, **f_locals}
-        finally:
-            del frame
-
-        df = run_query(query, context)
-        if self.scalar:
-            if len(df) > 1:
-                raise RuntimeError("Scalar query returned more than one row.")
-            if len(df.columns) > 1:
-                raise RuntimeError("Scalar query returned more than one column.")
-            return None if df.empty else df.iloc[0, 0]  # type: ignore
-        else:
-            return df  # type: ignore
+        pass
